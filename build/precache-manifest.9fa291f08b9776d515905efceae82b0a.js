be0710bcb5b77c78802e9c4c4f966222af742cd8
Dion Pinto <dionpinto000@gmail.com>
2020-06-04 13:31:23 +0530

    adding prod version of todo-react

---
+++ build/precache-manifest.9fa291f08b9776d515905efceae82b0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c05cb6591cfc22e4ee7da5a8f8f24e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "555099c4c34b38b54b85",
+    "url": "/static/css/main.114e331d.chunk.css"
+  },
+  {
+    "revision": "7777ed3f9f0b66bb64fa",
+    "url": "/static/js/2.791b52fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.791b52fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "555099c4c34b38b54b85",
+    "url": "/static/js/main.4a20d7b4.chunk.js"
+  },
+  {
+    "revision": "252f969779de0734efb3",
+    "url": "/static/js/runtime-main.75211130.js"
+  }
+]);
